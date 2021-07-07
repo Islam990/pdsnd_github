138c67c29db567026015fbbf9e3c9cddca88a55b
Eslam Gad <eslamhendy990@gmail.com>
2021-07-07 01:50:28 -0700

    add comment in line 5

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# map refer to csv file date of every city
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york': 'new_york_city.csv',
               'washington': 'washington.csv' }
